8a66905d845acef2c7010b4dd4fb7db2dece8ba7
BlueNet <luiyer1920@gmail.com>
2018-12-05 11:10:33 -0400

    creacion controladores

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('adm');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('/panel','PanelController');
 });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+// Route::get('/user',function(){
+// 	$user = new App\User;
+// 	$user->name = 'Luis Yerko';
+// 	$user->lastname ='Laura Tola';
+// 	$user->email = 'luis@gmail.com';
+// 	$user->password = bcrypt('123456');
+// 	$user->photo = 'foto.jpg';
+// 	$user->role = null;
+// 	$user->save();
+// 	return $user;
+// });
